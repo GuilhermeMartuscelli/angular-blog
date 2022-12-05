0337afa7a817b2e802857bf06a940afcb031a5a3
GuilhermeMartuscelli <luisg.rsantoss@gmail.com>
2022-12-05 11:50:34 -0300

    content criated

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    'id':'1',
+    'title':'Saiu nova versão angular!',
+    'description':'ola',
+    'photoCover':'https://i.pinimg.com/originals/b5/fe/11/b5fe11ee521e206fca92bc865d89c742.png'
+  },
+  {
+    'id':'2',
+    'title':'saiu novo filme',
+    'description':'ola',
+    'photoCover':'https://i.pinimg.com/originals/b5/fe/11/b5fe11ee521e206fca92bc865d89c742.png'
+  }
+]
+
